d1828de3e6df307af725ee9f266117e27d955d42
Kartik-Ranaut <ranautk6@gmail.com>
2024-02-04 23:09:08 +0530

    added fclike icon

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { FcLike } from "react-icons/fc";
 
 export default function Card(props) {
+  function btnhandler(event){
+    event.target.style.backgroundColor="red";
+  }
   return (
     <div className='w-3/12 h-6/12  flex  flex-col'>
         <img src={props.image.url} alt={props.image.alt}></img> <br></br>
         <h1 className='text-xl font-bold'>{props.title}</h1>
         <p className='text-sm text-justify'>{props.description}</p>
-        <button>
+        <button onClick={btnhandler}>
         <FcLike></FcLike>
         </button>
     </div>
